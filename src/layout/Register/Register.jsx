44adf756d9de0237aef33be5da34913a784fe2d1
Forhad <developerforhad2@gmail.com>
2024-07-26 10:19:56 +0600

    adding to skeleton and anding to register page

--- src/layout/Register/Register.jsx
+++ src/layout/Register/Register.jsx
@@ -1,11 +1,121 @@
+import { Button, Skeleton, Typography } from "@mui/material";
+import { useForm } from "react-hook-form";
+import useAuth from './../../hooks/useAuth';
 
 
 const Register = () => {
- return (
-  <div>
-   this is register pager
-  </div>
- );
+ const { loading } = useAuth();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => console.log(data);
+
+  return (
+    <div className="font-nunito">
+      <Typography>
+        {loading ? (
+          <Skeleton />
+        ) : (
+          <form onSubmit={handleSubmit(onSubmit)}>
+            {/* user name */}
+            <div className="flex flex-col gap-1 w-4/12">
+              <label>
+                <strong className="flex items-center gap-0 opacity-85">
+                  Name<span className="text-rose-500">*</span>
+                </strong>
+              </label>
+              <input
+                className="border outline-none py-2 px-1"
+                type="text"
+                placeholder="type your name*"
+                name="name"
+                {...register('name', { required: true })}
+                id=""
+              />
+              <span>
+                <small>
+                  <strong>
+                    {errors.name && (
+                      <strong className="text-red-600">
+                        This field in required
+                      </strong>
+                    )}
+                  </strong>
+                </small>
+              </span>
+            </div>
+            {/* user email */}
+            <div className="flex flex-col gap-1 w-4/12">
+              <label>
+                <strong className="flex items-center gap-0 opacity-85">
+                  Email<span className="text-rose-500">*</span>
+                </strong>
+              </label>
+              <input
+                className="border outline-none py-2 px-1"
+                type="email"
+                placeholder="Your email*"
+                name="name"
+                {...register('email', { required: true })}
+                id=""
+              />
+              <span>
+                <small>
+                  <strong>
+                    {errors.email && (
+                      <strong className="text-red-600">
+                        This field in required
+                      </strong>
+                    )}
+                  </strong>
+                </small>
+              </span>
+            </div>
+            {/* user password */}
+            <div className="flex flex-col gap-1 w-4/12">
+              <label>
+                <strong className="flex items-center gap-0 opacity-85">
+                  Password<span className="text-rose-500">*</span>
+                </strong>
+              </label>
+              <input
+                className="border outline-none py-2 px-1"
+                type="text"
+                placeholder="type your name*"
+                name="name"
+                {...register('password', { required: true })}
+                id=""
+              />
+              <span>
+                <small>
+                  <strong>
+                    {errors.password && (
+                      <strong className="text-red-600">
+                        This field in required
+                      </strong>
+                    )}
+                  </strong>
+                </small>
+              </span>
+            </div>
+            <div>
+              <Button
+                variant="contained"
+                className="w-4/12"
+                size="medium"
+                type="submit"
+              >
+                Register Now
+              </Button>
+            </div>
+          </form>
+        )}
+      </Typography>
+    </div>
+  );
 };
 
 export default Register;
--- src/page/Category/Category.jsx
+++ src/page/Category/Category.jsx
@@ -1,30 +1,42 @@
+import { useEffect } from "react";
+import { useState } from "react";
+import 'react-tooltip/dist/react-tooltip.css';
+import { Tooltip } from 'react-tooltip';
+import { Link } from "react-router-dom";
 
 
 const Category = () => {
- const category = [1, 2, 3, 4,5 ,6, 7, 8]
- const category2 = [1, 2, 3, 4,5 ,6,7,8]
+  const [category, setCategory] = useState([]);
+  useEffect(() => {
+    fetch('categories.json').then(res => res.json()).then(data => setCategory(data))
+  },[])
  return (
-   <div className="px-24 my-4 relative mt-12">
+   <div className="md:px-24 px-5 my-4 relative mt-12">
      <h1 className="text-3xl text-slate-900/90 font-semibold font-nunito">
        Categories
      </h1>
      <span className="absolute top-6 text-rose-700">=============</span>
-     <div className="grid grid-cols-8 text-balance  items-center mt-4">
+     <Tooltip id="items" />
+
+     <div className="grid md:grid-cols-8 grid-cols-1 text-balance  items-center mt-4">
        {category.map((item, index) => (
-         <div
+         <Link
            key={index}
-           className="border-x bg-slate-50 duration-300 hover:shadow-xl hover:drop-shadow-xl hover:border-slate-500/60 border-b h-48  flex flex-wrap md:flex-nowrap"
+           className="border-x bg-slate-50 duration-300 hover:shadow-xl hover:drop-shadow-xl hover:border-slate-500/60 border-b h-48 "
          >
-           name
-         </div>
-       ))}
-       {category2.map((item, index) => (
-         <div
-           key={index}
-           className="border-x bg-slate-50 duration-300 hover:shadow-xl hover:drop-shadow-xl hover:border-slate-500/60 border-b h-48  flex flex-wrap md:flex-nowrap"
-         >
-           name
-         </div>
+           <a data-tooltip-id="items" data-tooltip-content={item.name}>
+             <div className="w-full h-36 border-b-2 hover:border-b-black duration-200 border-b-slate-400 p-2">
+               <img
+                 className="w-full h-full border-4 hover:scale-105 duration-300 p-2 rounded-lg"
+                 src={item.image}
+                 alt={item.name}
+               />
+             </div>
+             <h1 className="font-nunito text-sm py-2 px-2 font-semibold">
+               {item.name}
+             </h1>
+           </a>
+         </Link>
        ))}
      </div>
    </div>
